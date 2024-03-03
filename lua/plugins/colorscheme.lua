40c6ab12a87d74595a10d741fb2ca1bf20501256
Martino Massalini <massalinimartino@gmail.com>
2024-03-03 19:17:22 +0100

    modifiche

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "cool",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
