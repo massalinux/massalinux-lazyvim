d4aeec12389aebb1abc1b0850a99c8cd97c14e8c
Martino Massalini <massalinimartino@gmail.com>
2024-03-03 18:53:30 +0100

    init

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      -- layout_strategy = "horizontal",
+      -- layout_config = { prompt_position = "top" },
+      -- sorting_strategy = "ascending",
+      winblend = 0,
+      file_ignore_patterns = {
+        "/node_modules/",
+        "node_modules/",
+        "^node_modules/",
+        "venv",
+        "package.json",
+        "package-lock.json",
+      },
+    },
+  },
+}
